e8cf30dbc604305c99ea307f8ea2e601d752cb19
Ramakrishna <rahulsur.suresh@gmail.com>
2021-01-25 20:17:11 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const mongoose = require('mongoose');
 
 const db = require('./db')
 const movieRouter = require('./routes/movie-router')
 
 const app = express()
 const apiPort = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/cinema', {
+    useNewParser: true,
+    useUnifiedTopology: true
+});
+
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
